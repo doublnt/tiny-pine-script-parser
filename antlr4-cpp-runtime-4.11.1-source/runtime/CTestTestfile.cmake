e6d1a685137aeb922f0475b7a812b8065f9a0931
doublnt <yinximail@gmail.com>
2022-12-10 15:37:54 +0800

    fix cmake build error.

--- antlr4-cpp-runtime-4.11.1-source/runtime/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/parrt/antlr/code/antlr4/runtime/Cpp/runtime
-# Build directory: /Users/parrt/antlr/code/antlr4/runtime/Cpp/runtime
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("/Users/parrt/antlr/code/antlr4/runtime/Cpp/runtime/antlr4_tests[1]_include.cmake")
-subdirs("../_deps/googletest-build")
--- main.cpp
+++ main.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
+#include <string>
 
-int main(int argc, char **argv)
+#include "antlr4-runtime.h"
+#include "ExprLexer.h"
+#include "ExprParser.h"
+
+int main(int argc, const char *argv[])
 {
-    std::cout << "Hello, World" << std::endl;
+    antlr4::ANTLRInputStream inputStream("1+1");
+    ExprLexer lexer(&inputStream);
+    antlr4::CommonTokenStream tokens(&lexer);
+    ExprParser parser(&tokens);
 
     return 0;
 }
